5bad17202fa31205aa96e5e8389983f931080404
Ilya <ilya@Ilyas-MacBook-Pro.local>
2014-11-20 11:49:48 +0300

    Minor bugs fixes

--- R/cmc.R
+++ R/cmc.R
@@ -1,13 +1,20 @@
-cmc <- function(filename) {
+cmc <- function(filename, method="fisher") {
   pgdata <- as.matrix(read.table(filename, as.is=T, skip = 1))
   y <- pgdata[,1];
   x <- ((rowSums(as.matrix(pgdata[,-1])))>0);
+  # Contigency table:
   m <- matrix(nrow=2,ncol=2);
   m[1,1] <- sum(x==1 & y==1);
   m[1,2] <- sum(x==0 & y==1);
   m[2,1] <- sum(x==1 & y==0);
   m[2,2] <- sum(x==0 & y==0);
   print(m);
-  stat <- fisher.test(m)$p.value;
+  if (method == "fisher") {
+    stat <- fisher.test(m)$p.value; # Fisher's exact test
+  }
+  if (method == "chisq") {
+    stat <- chisq.test(m)$p.value;
+  }
+  
   return(stat);
 }
