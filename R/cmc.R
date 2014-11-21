8b357d6725fe92104a40e3bb774d666eff5ecfe9
Ilya <ilya@Ilyas-MacBook-Pro.local>
2014-11-21 21:19:53 +0300

    Minor updates

--- R/cmc.R
+++ R/cmc.R
@@ -1,20 +1,25 @@
 cmc <- function(filename, method="fisher") {
+  # Input table consists of columns and rows. Rows are individuals and columns are:
+  # First column represents a status (disease / no disease) and other column represent markers.
   pgdata <- as.matrix(read.table(filename, as.is=T, skip = 1))
-  y <- pgdata[,1];
-  x <- ((rowSums(as.matrix(pgdata[,-1])))>0);
-  # Contigency table:
-  m <- matrix(nrow=2,ncol=2);
-  m[1,1] <- sum(x==1 & y==1);
-  m[1,2] <- sum(x==0 & y==1);
-  m[2,1] <- sum(x==1 & y==0);
-  m[2,2] <- sum(x==0 & y==0);
-  print(m);
+  markers <- pgdata[,1]; # Markers
+  status <- ((rowSums(as.matrix(pgdata[,-1])))>0);
+  
+  # Contingency table:
+  ct <- matrix(nrow=2,ncol=2);
+  ct[1,1] <- sum(markers==1 & status==1);
+  ct[1,2] <- sum(markers==0 & status==1);
+  ct[2,1] <- sum(markers==1 & status==0);
+  ct[2,2] <- sum(markers==0 & status==0);
+  
+  print(ct); # Let's take a look at the data
+  
   if (method == "fisher") {
-    stat <- fisher.test(m)$p.value; # Fisher's exact test
+    stat <- fisher.test(ct)$p.value; # Fisher's exact test
   }
   if (method == "chisq") {
-    stat <- chisq.test(m)$p.value;
+    stat <- chisq.test(ct)$p.value; # Should this test work?
   }
   
-  return(stat);
+  stat;
 }
